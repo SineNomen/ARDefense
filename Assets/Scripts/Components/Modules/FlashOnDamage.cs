a66fa806e4ed23a9735d5afa205fb6e2ee326a9d
Mathew Valadez <mat@foxcubgames.com>
2019-05-24 23:05:22 -0700

    Added prefab on destroy

---
+++ Assets/Scripts/Components/Modules/FlashOnDamage.cs
@@ -0,0 +1,49 @@
+using Sojourn.ARDefense.ScriptableObjects;
+using Sojourn.ARDefense.Interfaces;
+using Sojourn.ARDefense.Components;
+using Sojourn.PicnicIOC;
+using Sojourn.Extensions;
+using Sojourn.Utility;
+using UnityEngine;
+using AOFL.Promises.V1.Core;
+using AOFL.Promises.V1.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using DG.Tweening;
+
+namespace Sojourn.ARDefense.Components {
+	// [[RequireComponent (typeof (MeshRenderer))]]
+	public class FlashOnDamage : MonoBehaviour {
+		[SerializeField]
+		private Color _flashColor = Color.red;
+		[SerializeField]
+		private float _transitionTime = 0.1f;
+		[SerializeField]
+		private int _flashCount = 3;
+
+		private Renderer _renderer = null;
+		private Color _startColor = Color.white;
+		private Tween _flashTween = null;
+
+		// [AutoInject]
+		// private IGameManager _gameManager = null;
+
+		private void Awake() {
+			_renderer = GetComponent<Renderer>();
+			_startColor = _renderer.material.color;
+		}
+
+		[ContextMenu("Do Flash")]
+		public void TestDamage() { OnDamaged(null); }
+		public void OnDamaged(IKillable us) {
+			if (_flashTween != null) { _flashTween.Kill(); }
+			// _renderer.material.color = _startColor;
+
+			Sequence seq = DOTween.Sequence();
+			seq.Append(_renderer.material.DOColor(_flashColor, _transitionTime).SetEase(Ease.InSine));
+			seq.Append(_renderer.material.DOColor(_startColor, _transitionTime).SetEase(Ease.OutSine));
+			seq.SetLoops(_flashCount);
+		}
+	}
+}
---
+++ Assets/Scripts/Components/Modules/PrefabOnKilled.cs
@@ -0,0 +1,41 @@
+using Sojourn.ARDefense.ScriptableObjects;
+using Sojourn.ARDefense.Interfaces;
+using Sojourn.ARDefense.Components;
+using Sojourn.PicnicIOC;
+using Sojourn.Extensions;
+using Sojourn.Utility;
+using UnityEngine;
+using AOFL.Promises.V1.Core;
+using AOFL.Promises.V1.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using DG.Tweening;
+
+namespace Sojourn.ARDefense.Components {
+	// [[RequireComponent (typeof (MeshRenderer))]]
+	public class PrefabOnKilled : MonoBehaviour {
+		[SerializeField]
+		private GameObject _prefab = null;
+		[SerializeField]
+		private bool _preload = false;
+		private GameObject _preloadedObject = null;
+
+
+		private void Awake() {
+			if (_preload) {
+				_preloadedObject = Instantiate(_prefab, null);
+				_preloadedObject.SetActive(false);
+			}
+		}
+
+		[ContextMenu("Do Prefab")]
+		public void TestDamage() { OnKilled(null); }
+		public void OnKilled(IKillable us) {
+			GameObject obj = (_preload ? _preloadedObject : Instantiate(_prefab, null));
+			obj.transform.SetPositionAndRotation(this.transform.position, this.transform.rotation);
+			Debug.LogFormat("This: {0}, prefab: {1}", this.transform.position, obj.transform.position);
+			obj.SetActive(true);
+		}
+	}
+}
