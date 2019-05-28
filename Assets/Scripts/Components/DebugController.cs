764a13fedb70fbafac6d6f89dd87f9d1199ffc6f
Mathew Valadez <mat@foxcubgames.com>
2019-05-27 22:04:31 -0700

    Added debug controller and fixed base health meter

---
+++ Assets/Scripts/Components/DebugController.cs
@@ -0,0 +1,105 @@
+using Sojourn.ARDefense.ScriptableObjects;
+using Sojourn.ARDefense.Interfaces;
+using Sojourn.ARDefense.Components;
+using Sojourn.PicnicIOC;
+using Sojourn.Extensions;
+using Sojourn.Utility;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+using AOFL.Promises.V1.Core;
+using AOFL.Promises.V1.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+namespace Sojourn.ARDefense.Components {
+	public class DebugController : MonoBehaviour {
+		[SerializeField] private bool _useDebugControls = false;
+		[SerializeField] private float _moveSpeed = 1.0f;
+		[SerializeField] private float _rotateSpeed = 2.0f;
+		[SerializeField] private float _turboMultiplier = 2.50f;
+
+		[SerializeField] private KeyCode _forwardKey = KeyCode.W;
+		[SerializeField] private KeyCode _backKey = KeyCode.S;
+		[SerializeField] private KeyCode _strafeLeftKey = KeyCode.A;
+		[SerializeField] private KeyCode _strafeRightKey = KeyCode.D;
+		[SerializeField] private KeyCode _raiseKey = KeyCode.Q;
+		[SerializeField] private KeyCode _lowerKey = KeyCode.E;
+
+		[SerializeField] private KeyCode _turnLeftKey = KeyCode.LeftArrow;
+		[SerializeField] private KeyCode _turnRightKey = KeyCode.RightArrow;
+		[SerializeField] private KeyCode _turnUpKey = KeyCode.UpArrow;
+		[SerializeField] private KeyCode _turnDownKey = KeyCode.DownArrow;
+
+		[SerializeField] private KeyCode _resetOrientationKey = KeyCode.Escape;
+		[SerializeField] private KeyCode _resetAllKey = KeyCode.Space;
+		[SerializeField] private KeyCode _turboKey = KeyCode.LeftShift;
+		[SerializeField] private KeyCode _baseKey = KeyCode.B;
+
+		[AutoInject]
+		private IGameManager _gameManager = null;
+
+		private Quaternion _startingRotation;
+		private Vector3 _startingPosition;
+		private Transform _transform;
+		private bool _turboOn = false;
+
+
+		private void Start() {
+			Container.AutoInject(this);
+			if (_useDebugControls) {
+				Destroy(_gameManager.DeviceCamera.gameObject.GetComponent<ARCoreBackgroundRenderer>());
+				Destroy(_gameManager.DeviceCamera.gameObject.GetComponent<TrackedPoseDriver>());
+				_transform = _gameManager.DeviceCamera.transform;
+				_startingRotation = _gameManager.DeviceCamera.transform.rotation;
+				_startingPosition = _gameManager.DeviceCamera.transform.position;
+			}
+		}
+
+		private void Update() {
+			if (_useDebugControls) {
+				_turboOn = Input.GetKey(_turboKey);
+				if (Input.GetKey(_forwardKey)) { MovePosition(Vector3.forward); }
+				if (Input.GetKey(_backKey)) { MovePosition(Vector3.back); }
+				if (Input.GetKey(_strafeLeftKey)) { MovePosition(Vector3.left); }
+				if (Input.GetKey(_strafeRightKey)) { MovePosition(Vector3.right); }
+				if (Input.GetKey(_raiseKey)) { MovePosition(Vector3.up); }
+				if (Input.GetKey(_lowerKey)) { MovePosition(Vector3.down); }
+
+				if (Input.GetKey(_turnLeftKey)) { MoveRotation(Vector3.down); }
+				if (Input.GetKey(_turnRightKey)) { MoveRotation(Vector3.up); }
+				if (Input.GetKey(_turnUpKey)) { MoveRotation(Vector3.right); }
+				if (Input.GetKey(_turnDownKey)) { MoveRotation(Vector3.left); }
+
+				if (Input.GetKey(_baseKey)) { _transform.LookAt(_gameManager.Player1Base.transform); }
+				if (Input.GetKey(_resetOrientationKey)) { _transform.rotation = _startingRotation; }
+				if (Input.GetKey(_resetAllKey)) {
+					_transform.rotation = _startingRotation;
+					_transform.position = _startingPosition;
+				}
+			}
+		}
+
+		private void MovePosition(Vector3 adjust) {
+			Vector3 delta = Vector3.zero;
+			if (_turboOn) {
+				delta = adjust * _moveSpeed * _turboMultiplier;
+			} else {
+				delta = adjust * _moveSpeed;
+			}
+			_transform.Translate(delta);
+		}
+
+		private void MoveRotation(Vector3 adjust) {
+			Vector3 rot = Vector3.zero;
+			if (_turboOn) {
+				rot = adjust * _rotateSpeed * _turboMultiplier;
+			} else {
+				rot = adjust * _rotateSpeed;
+			}
+			_transform.Rotate(rot);
+		}
+	}
+}
+#endif// UNITY_EDITOR || UNITY_STANDALONE
